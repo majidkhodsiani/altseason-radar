d84165720bfd730566eef3440cec95abd373887a
majidkhodsiani <majid.khodsiani@gmail.com>
2026-07-08 14:37:41 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from telegram_bot import start_bot
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 
-if __name__ == "__main__":
-
-    if not TOKEN:
-        print("Telegram token missing")
-    else:
-        start_bot(TOKEN)
+if TOKEN:
+    print("🤖 Altseason Radar started")
+    start_bot(TOKEN)
+else:
+    print("❌ TELEGRAM_TOKEN not found")
